dd439713e8f1425210823fa297090e6b7878f998
Dean Packard <dpackard@guidecx.com>
2021-08-24 09:28:32 -0600

    handle sentences

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,13 @@ import * as functions from "firebase-functions";
 // Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
 const { WebClient, LogLevel } = require("@slack/web-api");
 
+// WebClient insantiates a client that can call API methods
+// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+const client = new WebClient("xoxb-623399190916-2417229728724-GjVHGB6JJYqhUYrZEGfePBNi", {
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG
+});
+
 const nlp = require('compromise');
 nlp.extend(require('compromise-sentences'));
 
@@ -34,6 +41,7 @@ const cleanPhrase = (subject: string, object: string, verb: string) => {
 
 const getYodifiedMessage = (originalMessage: string) => {
     const message = originalMessage.toLowerCase();
+    console.log(message);
     const subject = nlp(message).sentences().subjects().text();
 
     const verb = nlp(message).verbs().text();
@@ -43,22 +51,25 @@ const getYodifiedMessage = (originalMessage: string) => {
         .replace(RegExp(`\\b(${subject})\\b`), '');
 
     if (isSubjectPronoun(subject)) {
+      if(object.trim() === '' || !object) {
+        return 'Not well, I was coded';
+      }
         return cleanPhrase(object, subject, verb);
     } else {
         return cleanPhrase(subject, object, verb);
     }
 };
 
-// WebClient insantiates a client that can call API methods
-// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient("xoxb-623399190916-2417229728724-WAoVhfYD9lShU5VzOfhpBPth", {
-  // LogLevel can be imported and used to make debugging simpler
-  logLevel: LogLevel.DEBUG
-});
-
 export const yodifyMessage = functions.https.onRequest(async (req, res) => {
   try {
-    let text = getYodifiedMessage(req.body.text);
+    let removedAt = req.body.text.replace('@', '');
+    let removedComma = removedAt.replace(',', '');
+    //split into sentences
+    let sentencesArray = removedComma.split('?').join('.').split('!').join('.').split('.');
+    let text = '';
+    sentencesArray.forEach((sentence: string) => {
+      text += getYodifiedMessage(sentence) + '. ';
+    });
     // Call the chat.postMessage method using the WebClient
     await client.chat.postMessage({
       channel: `${req.body.channel_id}`,
@@ -68,6 +79,7 @@ export const yodifyMessage = functions.https.onRequest(async (req, res) => {
     res.status(200).send();
   }
   catch (error) {
+    console.log(error)
     res.status(400).send();
   }
 });
